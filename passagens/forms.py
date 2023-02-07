d38fde11375dd0545e838f43f593e499126364ad
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2023-02-07 01:14:18 -0300

    atualização

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classes
+from passagens.validation import *
 
 class PassaegmForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
@@ -17,10 +18,19 @@ class PassaegmForms(forms.Form):
     )
     email = forms.EmailField(label='Email', max_length=150)
 
-    def clean_origem(self):
+    def clean(self):
         origem = self.cleaned_data.get('origem')
+        destino = self.cleaned_data.get('destino')
+        lista_de_erros = {}
 
-        if any(char.isdigit() for char in origem):
-            raise forms.ValidationError('Origem inválida: Não inclua números')
-        else:
-            return origem
+        campo_numerico(origem, 'origem', lista_de_erros)
+        campo_numerico(destino, 'destino', lista_de_erros)
+        origem_destino_iguais(origem, destino, lista_de_erros)
+
+        if lista_de_erros is not None:
+            for erro in lista_de_erros:
+                mensagem_erro = lista_de_erros[erro]
+                self.add.error(erro, mensagem_erro)
+
+        return self.cleaned_data
+
---
+++ passagens/validation.py
@@ -0,0 +1,8 @@
+def origem_destino_iguais(origem, destino, lista_de_erros):
+    if origem == destino:
+        lista_de_erros['destino'] = 'Origem e destino não podem ser iguais'
+
+def campo_numerico(valor_campo, nome_campo, lista_de_erros):
+    if any(char.isdigit() for char in valor_campo):
+        lista_de_erros[nome_campo] = 'Não inclua números neste campo'
+
