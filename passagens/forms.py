043fa2e04e209a5e457ced226ed7ebc4d0dbb189
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2023-02-07 00:49:32 -0300

    atualização

--- passagens/forms.py
+++ passagens/forms.py
@@ -16,3 +16,11 @@ class PassaegmForms(forms.Form):
         widget=forms.Textarea()
     )
     email = forms.EmailField(label='Email', max_length=150)
+
+    def clean_origem(self):
+        origem = self.cleaned_data.get('origem')
+
+        if any(char.isdigit() for char in origem):
+            raise forms.ValidationError('Origem inválida: Não inclua números')
+        else:
+            return origem
